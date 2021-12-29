7d76aa0966d4b1cf4b0fcf9d1277768db95d310a
AquaFoxCoding <89033364+AquaFoxCoding@users.noreply.github.com>
2021-12-29 15:04:15 -0600

    Added CMS

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/img");
   eleventyConfig.addPassthroughCopy("./src/sass");
   eleventyConfig.addPassthroughCopy("./src/script.js");
+  eleventyConfig.addPassthroughCopy("./src/admin");
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
   });
